899e495a7a2e2fdc8fbba0ecf75d30299349011a
Muchiachio <aldis.rameika@gmail.com>
2016-08-01 19:56:37 +0300

    Database context based lookup examples
    - Added context injection examples, to make lookup examples more "real
    world usage" like.

--- src/Mvc.Lookup.Web/Controllers/MvcAutocompleteController.cs
+++ src/Mvc.Lookup.Web/Controllers/MvcAutocompleteController.cs
@@ -10,14 +10,7 @@ namespace NonFactors.Mvc.Lookup.Web.Controllers
         {
             filter.AdditionalFilters.Add("Age", autocompleteAge);
 
-            return GetData(new PeopleAutocomplete(), filter);
-        }
-
-        private JsonResult GetData(MvcLookup lookup, LookupFilter filter)
-        {
-            lookup.Filter = filter;
-
-            return Json(lookup.GetData());
+            return Json(new PeopleAutocomplete() { Filter = filter }.GetData());
         }
     }
 }
--- src/Mvc.Lookup.Web/Controllers/MvcLookupController.cs
+++ src/Mvc.Lookup.Web/Controllers/MvcLookupController.cs
@@ -10,14 +10,7 @@ namespace NonFactors.Mvc.Lookup.Web.Controllers
         {
             filter.AdditionalFilters.Add("Age", lookupAge);
 
-            return GetData(new PeopleLookup(), filter);
-        }
-
-        private JsonResult GetData(MvcLookup lookup, LookupFilter filter)
-        {
-            lookup.Filter = filter;
-
-            return Json(lookup.GetData());
+            return Json(new PeopleLookup() { Filter = filter }.GetData());
         }
     }
 }
